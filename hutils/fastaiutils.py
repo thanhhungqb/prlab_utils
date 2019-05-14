8ebd7c9d5a542cd4add64dfe3997913f11a5c193
hung <thanhhungqb@gmail.com>
2019-05-14 14:12:11 +0900

    add some use full funcions: losses, metrics

--- hutils/fastaiutils.py
+++ hutils/fastaiutils.py
@@ -1,5 +1,9 @@
+import numpy as np
+import torch
 from fastai import callbacks
 from fastai.callbacks import SaveModelCallback, partial
+from torch.autograd import Variable
+from torch.nn.functional import log_softmax
 
 
 def get_callbacks(best_name='best', monitor='accuracy', csv_filename='log', csv_append=True):
@@ -9,3 +13,84 @@ def get_callbacks(best_name='best', monitor='accuracy', csv_filename='log', csv_
         partial(callbacks.CSVLogger, filename=csv_filename, append=csv_append)
     ]
     return out
+
+
+def dificult_weight_loss(y_preds, target, *args, **kwargs):
+    """
+    loss = - 1 * log(pi) * (pj/pi), where pj is max(p_)
+    :param y_preds:
+    :param target:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    softmax = log_softmax(y_preds, 1)
+    target_one_hot = torch.zeros(len(target.cpu()), 7).scatter_(1, target.cpu().unsqueeze(1), 1.)
+
+    xa, _ = torch.max(softmax, dim=1)
+
+    a = -target_one_hot.cuda() * softmax
+    a = torch.sum(a, dim=1)
+
+    xaa = 1 + xa + a
+    a = a * xaa
+    return torch.mean(a)
+
+
+def make_one_hot(labels, C=2):
+    """
+    credit: https://gist.github.com/jacobkimmel/4ccdc682a45662e514997f724297f39f
+    Converts an integer label torch.autograd.Variable to a one-hot Variable.
+
+    Parameters
+    ----------
+    labels : torch.autograd.Variable of torch.cuda.LongTensor
+        N x 1 x H x W, where N is batch size.
+        Each value is an integer representing correct classification.
+    C : integer.
+        number of classes in labels.
+
+    Returns
+    -------
+    target : torch.autograd.Variable of torch.cuda.FloatTensor
+        N x C x H x W, where C is class number. One-hot encoded.
+    """
+    one_hot = torch.cuda.FloatTensor(labels.size(0), C, labels.size(2), labels.size(3)).zero_()
+    target = one_hot.scatter_(1, labels.data, 1)
+
+    target = Variable(target)
+
+    return target
+
+
+def dice_loss(input, target):
+    """
+    credit: https://github.com/rogertrullo/pytorch/blob/rogertrullo-dice_loss/torch/nn/functional.py#L708
+    input is a torch variable of size BatchxnclassesxHxW representing log probabilities for each class
+    target is a 1-hot representation of the groundtruth, shoud have same size as the input
+    """
+    target = make_one_hot(target, C=2)
+    assert input.size() == target.size(), "Input sizes must be equal."
+    assert input.dim() == 4, "Input must be a 4D Tensor."
+    uniques = np.unique(target.cpu().numpy())
+    assert set(list(uniques)) <= set([0, 1]), "target must only contain zeros and ones"
+
+    probs = torch.softmax(input, dim=-1)
+    num = probs * target  # b,c,h,w--p*g
+    num = torch.sum(num, dim=3)  # b,c,h
+    num = torch.sum(num, dim=2)
+
+    den1 = probs * probs  # --p^2
+    den1 = torch.sum(den1, dim=3)  # b,c,h
+    den1 = torch.sum(den1, dim=2)
+
+    den2 = target * target  # --g^2
+    den2 = torch.sum(den2, dim=3)  # b,c,h
+    den2 = torch.sum(den2, dim=2)  # b,c
+
+    dice = 2 * (num / (den1 + den2))
+    dice_eso = dice[:, 1:]  # we ignore bg dice val, and take the fg
+
+    dice_total = -1 * torch.sum(dice_eso) / dice_eso.size(0)  # divide by batch_sz
+
+    return dice_total
