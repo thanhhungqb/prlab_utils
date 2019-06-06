928c5ab1a55e26c22c6f814fd0c5abda3d5614ed
hung <thanhhungqb@gmail.com>
2019-06-06 10:13:15 +0900

    some wide use functions

---
+++ prlab/gutils.py
@@ -0,0 +1,44 @@
+import time
+from pathlib import Path
+
+import numpy as np
+from sklearn import preprocessing
+
+
+def make_check_point_folder(path, config={}, cp_name=None):
+    """
+    make a checkpoint folder and csv log
+    :param path:
+    :param config:
+    :param cp_name:
+    :return: path of cp, best cp, csv file path
+    """
+    path = path if isinstance(path, Path) else Path(path)
+    if cp_name is None:
+        cp_name = "{}".format(time.strftime("%Y.%m.%d-%H.%M"))
+    cp_path = path / cp_name
+    best_name = cp_path / "best"
+    csvLog = cp_path / "loger.csv"
+
+    return cp_path, best_name, csvLog
+
+
+def map_str_prob(str_prob):
+    """
+    Mostly load column prob from csv or text
+    :param str_prob: '[5, 3, 0, 1, 1]'
+    :return: [0.5 0.3 0 .1 .1]
+    """
+    s = [o.replace('[', '').replace(']', '').split() for o in str_prob]
+    f = [[float(o) for o in row] for row in s]
+    f = np.array(f)
+    fn = preprocessing.normalize(f, axis=1, norm='l1')
+    return fn
+
+
+def test_make_check_point_folder():
+    print(make_check_point_folder("/tmp"))
+
+
+if __name__ == '__main__':
+    test_make_check_point_folder()
