69044f4133bcdaf27aa88a53573043697e6f03ae
hung <thanhhungqb@gmail.com>
2019-06-10 21:57:41 +0900

    fix VideoFramesItem data value (must be Tensor)

--- prlab/fastai/video_data.py
+++ prlab/fastai/video_data.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+import numpy as np
+import torch
 from fastai.core import ItemBase
-from fastai.vision import ImageList, open_image, Image
+from fastai.vision import ImageList, open_image, Image, crop_pad
 from torch import Tensor
 
 """
@@ -33,13 +35,13 @@ class VideoFramesItem(ItemBase):
 
     def __init__(self, *imgs):
         self.imgs = list(imgs)
-        self.obj, self.data = imgs, [img.data for img in self.imgs]
+        self.obj, self.data = imgs, torch.stack([img.data for img in self.imgs])
 
     def apply_tfms(self, tfms, **kwargs):
         for i in range(len(self.imgs)):
             self.imgs[i] = self.imgs[i].apply_tfms(tfms, **kwargs)
 
-        self.data = [img.data for img in self.imgs]
+        self.data = torch.stack([img.data for img in self.imgs])
         return self
 
     def __repr__(self) -> str: return f'{self.__class__.__name__}'
@@ -85,3 +87,53 @@ class VideoFramesList(ImageList):
     def reconstruct(self, t: Tensor):
         imgs = [Image(t[i]) for i in range(self.n_frame)]
         return VideoFramesItem(*imgs)
+
+
+class CropImageList(ImageList):
+    """
+    Load and crop image on the fly.
+    Use Dataframe index: name, bb: [left top right bottom]
+    """
+
+    def __init__(self, items, df=None, scale_type='fixed', max_scale=1.2, min_scale=1., **kwargs):
+        super().__init__(items, **kwargs)
+        self.df = df
+
+        self.scale_type = scale_type
+        self.max_scale = max_scale
+        self.min_scale = min_scale
+
+    def get(self, i):
+        img = super().get(i)
+        item = self.items[i]
+
+        # Crop by info from self.df
+        item_path = item if isinstance(item, Path) else Path(item)
+        img_width, img_heigh = img.size
+
+        left, top, right, bottom = self.df.loc[item_path.name, 'bb']
+
+        # select area to crop
+        center_x, center_y = (left + right) / 2, (top + bottom) / 2
+        dx, dy = (right - left) / 2, (bottom - top) / 2
+
+        if self.scale_type == 'fixed':
+            left, right = center_x - dx * self.max_scale, center_x + dx * self.max_scale
+            top, bottom = center_y - dy * self.max_scale, center_y + dy * self.max_scale
+
+        if self.scale_type == 'random':
+            s_zoom = np.random.uniform(self.min_scale, self.max_scale, 4)
+
+            # TODO random each value in min-max
+            left, right = center_x - dx * s_zoom[0], center_x + dx * s_zoom[1]
+            top, bottom = center_y - dy * s_zoom[2], center_y + dy * s_zoom[3]
+
+        # refine if out of size
+        left, top, right, bottom = max(1, left), max(1, top), min(img_width - 1, right), min(img_heigh - 1, bottom)
+        size = int(bottom - top), int(right - left)
+        center = (bottom + top) / 2 / img_heigh, (right + left) / 2 / img_width
+
+        # cropped = img.crop((left, top, right, bottom))
+        cropped = crop_pad(img, size=size, row_pct=center[0], col_pct=center[1])
+
+        return cropped
