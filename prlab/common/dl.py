5b812f7b772608e721cec7beb61f52f609295ee9
hung <thanhhungqb@gmail.com>
2020-10-06 21:16:25 +0900

    add make_train_loggers

--- prlab/common/dl.py
+++ prlab/common/dl.py
@@ -1,8 +1,11 @@
+import logging
 from pathlib import Path
 
+from prlab.common.logger import PrettyLineHandler, WandbHandler
 from prlab.gutils import make_check_point_folder, convert_to_obj_or_fn
 
 
+# ========================  GENERAL ========================
 def general_dl_make_up(**config):
     """
     Widely used for basic configure for deep learning train/test
@@ -22,3 +25,25 @@ def general_dl_make_up(**config):
     })
 
     return config
+
+
+def make_train_loggers(**config):
+    """ There are two logger including general logger to stdout/file and another to progress (stdout/wandb)"""
+    name = config.get('run', 'general')
+    proj_name = config.get('proj_name', name)
+    log_level = config.get('log_level', logging.INFO)
+
+    logger_general = logging.getLogger(name)
+    logger_general.setLevel(log_level)
+    logger_general.addHandler(logging.StreamHandler())
+    logger_general.addHandler(logging.FileHandler(config["cp"] / "general.log"))
+
+    logger_progress = logging.getLogger(f"{name}_progress")
+    logger_progress.setLevel(log_level)
+    logger_progress.addHandler(PrettyLineHandler())
+    logger_progress.addHandler(PrettyLineHandler(base_hdl=logging.FileHandler(config["cp"] / "progress.log")))
+    if config.get('wandb') is not None:
+        logger_progress.addHandler(WandbHandler(proj_name=proj_name))
+
+    return {**config, 'train_logger': logger_general, 'progress_logger': logger_progress}
+# ========================  END OF GENERAL ========================
