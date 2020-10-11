1907342665123ea7c5817a8bfa84eca63d79d60d
hung <thanhhungqb@gmail.com>
2020-10-11 22:27:29 +0900

    common setup after model created

--- prlab/torch/pipeline.py
+++ prlab/torch/pipeline.py
@@ -22,3 +22,16 @@ def opt_func_load(**config):
     opt_fn = convert_to_obj_or_fn(config['opt_func'])
     config['opt_func'] = opt_fn(params=config['model'].parameters(), lr=config.get('lr'))
     return config
+
+
+def model_general_setup(**config):
+    """
+    General setup after model build, including opt_func, loss_func, metrics
+    :param config:
+    :return:
+    """
+    config['opt_func'] = convert_to_obj_or_fn(config['opt_func'], params=config['model'].parameters())
+    config['loss_func'] = convert_to_obj_or_fn(config['loss_func'], **config)
+    config['metrics'] = convert_to_obj_or_fn(config['metrics'], **config)
+
+    return config
