db8c3edf4bac2188adc667d8aacf6e68017d9e43
Hung Vo <thanhhungqb@gmail.com>
2021-02-15 22:03:26 +0900

    write json to file

--- prlab/torch/utils.py
+++ prlab/torch/utils.py
@@ -232,3 +232,10 @@ def seed_set(**config):
         torch.manual_seed(seed)
         np.random.seed(seed)
     return config
+
+
+def write_config_json(**config):
+    json_name = config.get('json_name', 'configure.json')
+    with open(config['cp'] / json_name, 'w') as fw:
+        json.dump(to_json_writeable(config), fw, indent=2)
+    return config
